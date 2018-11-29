c5fe8568e3c1c225014ad3c5d76ab7adc9489b8a
ruko <rukokarasu@gmail.com>
2018-11-28 19:43:14 -0800

    Test backquotes, vectors, hashes, identifiers

--- test/ElispParse/ElispParserSpec.hs
+++ test/ElispParse/ElispParserSpec.hs
@@ -11,6 +11,7 @@ import           Test.Hspec
 import           Test.Hspec.Megaparsec
 import           Text.Megaparsec               as M
 import           Text.Megaparsec.Char
+import qualified Data.Vector                   as V
 import           Data.Void
 import qualified Data.Text                     as T
 
@@ -33,7 +34,7 @@ spec = do
                 shouldParse (runParseProgram
                     "'( (1 2 3) (1 2 3) (1 2 3))")
                     (ASTQuote $ replicate 3 (ASTList oneTwoThree))
-        
+
         let fourFiveSix = ASTInt <$> [4,5,6] in do
             it "parses lists of expressions" $ do
                 shouldParse (runParseProgram
@@ -43,4 +44,37 @@ spec = do
                 shouldParse (runParseProgram
                     "(   (4 5 6) (4 5 6) (4 5 6) )")
                     (ASTList $ replicate 3 (ASTList fourFiveSix))
-        
+
+        do
+            it "parses backquoted expressions" $ do
+                shouldParse (runParseProgram
+                    "`(1 ,2 3)")
+                    (ASTBackquote
+                         [ Quoted (ASTInt 1)
+                         , Unquoted (ASTInt 2)
+                         , Quoted (ASTInt 3)
+                         ])
+
+        let emptyVector = ASTVector (HashableVector V.empty)
+            zeroOneTwo = ASTVector (HashableVector (V.generate 3 ASTInt)) in do
+            it "parses the empty vector" $ do
+                shouldParse (runParseProgram
+                    "[]")
+                    emptyVector
+            it "parses non-empty vectors" $ do
+                shouldParse (runParseProgram
+                    "[0 1 2]")
+                    zeroOneTwo
+
+        let hashConstructor = ASTIdentifier (Identifier "hash-table")
+            table = ASTTable (hashConstructor : map ASTInt [1, 10, 2, 20]) in do
+            it "parses tables" $ do
+                shouldParse (runParseProgram
+                    "#s(hash-table 1 10 2 20)")
+                    table
+
+        do
+            it "parses identifiers" $ do
+                shouldParse (runParseProgram
+                    "garfield")
+                    (ASTIdentifier (Identifier "garfield"))
