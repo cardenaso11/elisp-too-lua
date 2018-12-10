ac6c9f06933f2f659163fd14df2e21fde0adea51
card <cardenaso11@users.noreply.github.com>
2018-12-09 23:57:10 -0800

    removed some useless files & added project info to Main

--- ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for elisp-too-lua
-
-## Unreleased changes
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Lib
-
 main :: IO ()
-main = someFunc
+main = putStrLn
+  "main executable not yet implemented\n\
+  \when it is done, expect to see an elisp to lua compiler\
+  \and a way to run luajit on elisp transpiled in memory"
