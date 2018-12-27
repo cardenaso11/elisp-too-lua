9d2aad12f7ddbdf637ead0ae4aeb37d7537d9a0f
Nicholas Cardenas <nicholas.cardenas.1@gmail.com>
2018-12-27 00:59:02 -0800

    added very abbreviated synonyms for test brevity

--- test/ElispParse/TestCommon.hs
+++ test/ElispParse/TestCommon.hs
@@ -1,8 +1,27 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 module ElispParse.TestCommon
   ( shouldParse'
+  , pattern FAL
+  , pattern FAQ
+  , pattern FABQ
+  , pattern FAV
+  , pattern FAT
+  , pattern FACo
+  , pattern FACT
+  , pattern FACTST
+  , pattern FABC
+  , pattern FAId
+  , pattern FAId_
+  , pattern FAF
+  , pattern FAIn
+  , pattern FACh
+  , pattern FAS
+  , pattern FABV
+  , pattern AId_
   ) where
 
 import Test.Hspec
@@ -18,3 +37,23 @@ import ElispParse.NumberParser
 
 shouldParse' :: Either (ParseError Char Void) InfiniteAST -> InfiniteAST -> Expectation
 shouldParse' = shouldParse
+
+-- abbreviated pattern synonyms to make tests legible
+pattern FAL x = FASTList x
+pattern FAQ x = FASTQuote x
+pattern FABQ x = FASTBackquote x
+pattern FAV x = FASTVector x
+pattern FAT x = FASTTable x
+pattern FACo x xs = FASTCons x xs
+pattern FACT x = FASTCharTable x
+pattern FACTST x = FASTCharSubTable x
+pattern FABC x = FASTByteCode x
+pattern FAId x = FASTIdentifier x
+pattern FAId_ x = FASTIdentifier_ x
+pattern FAF x = FASTFloat x
+pattern FAIn x = FASTInt x
+pattern FACh x = FASTChar x
+pattern FAS x = FASTString x
+pattern FABV x y = FASTBoolVector x y
+
+pattern AId_ x = ASTIdentifier_ x
