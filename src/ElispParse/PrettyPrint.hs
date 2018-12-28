ecb522ace69d1bfb5aa7a31a6369e942a9677df8
Ethan Jones <rukokarasu@gmail.com>
2018-12-27 18:19:51 -0800

    Complete pretty definition for all AST variants (#25)

--- src/ElispParse/PrettyPrint.hs
+++ src/ElispParse/PrettyPrint.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -6,6 +7,7 @@ module ElispParse.PrettyPrint where
 
 import ElispParse.Common
 import Data.Foldable
+import qualified Data.Text.Lazy as T
 import Data.Text.Prettyprint.Doc
 
 instance Pretty (f (Fix f)) => Pretty (Fix f) where
@@ -13,16 +15,41 @@ instance Pretty (f (Fix f)) => Pretty (Fix f) where
 
 instance Pretty a => Pretty (AST a) where
     pretty = \case
-        ASTList xs     -> "(" <> hsep (pretty <$> xs) <> ")"
-        ASTQuote x     -> "'" <> pretty x
-        ASTBackquote x -> "`" <> pretty x
-        ASTInt n       -> pretty n
-        ASTChar c      -> "'" <> pretty c <> "'"
-        ASTString s    -> "\"" <> pretty s <> "\""
+        ASTList xs                   ->          parens xs
+        ASTQuote x                   -> "'"   <> pretty x
+        ASTBackquote x               -> "`"   <> pretty x
+        ASTVector (HashableVector v) ->          brackets (toList v)
+        ASTTable xs                  -> "#s"  <> parens xs
+        ASTCons xs x                 ->          cons xs x
+        ASTCharTable xs              -> "#^"  <> brackets xs
+        ASTCharSubTable xs           -> "#^^" <> brackets xs
+        ASTByteCode xs               -> "#"   <> brackets xs
         ASTIdentifier (Identifier i) -> pretty i
+        ASTFloat f                   -> pretty f
+        ASTInt n                     -> pretty n
+        ASTChar c                    -> char c
+        ASTString s                  -> escapedText s
+        ASTBoolVector n t            -> "#&" <> pretty n <> escapedText t
+        where
+            parens xs   = "(" <> hsep (pretty <$> xs) <> ")"
+            brackets xs = "[" <> hsep (pretty <$> xs) <> "]"
+            escapedText s = "\"" <> pretty (escapeText s) <> "\""
+            char c = "?" <> pretty c
+            cons xs x = "(" <> hsep (fmap pretty xs ++ [".", pretty x]) <> ")"
 
 instance Pretty a => Pretty (BackquotedAST a) where
     pretty = \case
         Quoted   ast -> pretty ast
         Unquoted ast -> "," <> pretty ast
         Spliced  ast -> ",@" <> pretty ast
+
+-- | Produce a Doc from a complete elisp program by printing each top-level
+-- declaration on a new line.
+program :: InfiniteAST -> Maybe (Doc a)
+program (FASTList xs) = Just (vsep (pretty <$> xs))
+program _            = Nothing
+
+escapeText :: T.Text -> T.Text
+escapeText = T.concatMap \case
+    '"' -> "\\\""
+    c   -> T.singleton c
--- test/ElispParse/PrettyPrintSpec.hs
+++ test/ElispParse/PrettyPrintSpec.hs
@@ -24,8 +24,32 @@ spec = do
         it "produces pretty function definitions" $ do
             let func = "(defun sq (n &optional foo &rest bar) (* n n))"
             shouldRender func
+        it "handles vectors" $ do
+            let ex = "[1 2 3]"
+            shouldRender ex
+        it "handles chars" $ do
+            let char = "?a"
+            shouldRender char
+            show (pretty (FASTChar 'a')) `shouldBe` "?a"
+        it "handles strings" $ do
+            show (pretty (FASTString "awoo")) `shouldBe` "\"awoo\""
+        it "escapes strings" $ do
+            show (pretty (FASTString "\"awoo\"")) `shouldBe` "\"\\\"awoo\\\"\""
+        it "handles cons cells" $ do
+            show (pretty (FASTCons [FASTInt 1] (FASTInt 2))) `shouldBe` "(1 . 2)"
+        it "handles floats" $ do
+            show (pretty (FASTFloat 2.71)) `shouldBe` "2.71"
+    describe "program" $ do
+        it "recognizes ASTs that represent programs" $ do
+            let code = "(defun foo () 1)\n(defun bar () 2)"
+            let ast =
+                    FAL
+                      [ FAL [ FAId_ "defun", FAId_ "foo", FAL [], FAIn 1 ]
+                      , FAL [ FAId_ "defun", FAId_ "bar", FAL [], FAIn 2 ]
+                      ]
+            fmap show (program ast) `shouldBe` Just code
 
 shouldRender :: T.Text -> Expectation
 shouldRender ex =
     let render = renderLazy . layoutPretty defaultLayoutOptions . pretty
-    in  fmap render (parseText exprFP ex) `shouldBe` Right (render ex)
+    in  fmap render (parseText exprFP ex) `shouldBe` Right ex
