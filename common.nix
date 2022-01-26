3134ca291c121fc7c084e463504c07c8b2203ec8
card <cardenaso11@users.noreply.github.com>
2022-01-25 17:55:35 -0800

    pin nixpkgs and fix build

--- common.nix
+++ common.nix
@@ -1,11 +1,16 @@
-{ pkgsPath ? <nixpkgs> }:
+{
+  pkgsPath ? builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/3fa154fd7fed3d6a94322bf08a6def47d6f8e0f6.tar.gz";
+
+  }
+}:
 
 {
   pkgs = import pkgsPath {
     overlays =
       let projectsOverlay =
         self: super: {
-          haskellPackages = super.haskell.packages.ghc862.extend (
+          haskellPackages = super.haskell.packages.ghc864.extend (
             super.haskell.lib.packageSourceOverrides {
               elisp-too-lua = ./.;
             }
@@ -14,8 +19,9 @@
           dontTestOverlay = 
             self: super: {
               haskellPackages = super.haskellPackages.extend (innerSelf: innerSuper:
-                { generic-lens = super.haskell.lib.dontCheck innerSuper.generic-lens; }
-                );
+              {
+                generic-lens = super.haskell.lib.dontCheck innerSuper.generic-lens;
+              });
             };
         
 
